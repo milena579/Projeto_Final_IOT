8dedff922128bbac5d2929874d7d0c92b9f8edb6
Milena <100636013+milena579@users.noreply.github.com>
2024-11-12 16:48:04 -0300

    Create codigoRFID.cpp

---
+++ codigoRFID.cpp
@@ -0,0 +1,48 @@
+// https://www.instructables.com/Interfacing-RFID-RC522-With-Arduino-MEGA-a-Simple-/
+// https://blogmasterwalkershop.com.br/arduino/como-usar-com-arduino-kit-rfid-mfrc522
+
+
+#include <deprecated.h>
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <require_cpp11.h>
+
+#include <SPI.h> //INCLUSÃO DE BIBLIOTECA
+#include <MFRC522.h> //INCLUSÃO DE BIBLIOTECA
+
+#define SS_PIN 10 //PINO SDA
+#define RST_PIN 9 //PINO DE RESET
+
+MFRC522 rfid(SS_PIN, RST_PIN); //PASSAGEM DE PARÂMETROS REFERENTE AOS PINOS
+
+int buzzer = 2;
+void setup() {
+  Serial.begin(9600); //INICIALIZA A SERIAL
+  SPI.begin(); //INICIALIZA O BARRAMENTO SPI
+  rfid.PCD_Init(); //INICIALIZA MFRC522
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) //VERIFICA SE O CARTÃO PRESENTE NO LEITOR É DIFERENTE DO ÚLTIMO CARTÃO LIDO. CASO NÃO SEJA, FAZ
+    return; //RETORNA PARA LER NOVAMENTE
+
+  /***INICIO BLOCO DE CÓDIGO RESPONSÁVEL POR GERAR A TAG RFID LIDA***/
+  String strID = "";
+  for (byte i = 0; i < 4; i++) {
+    strID +=
+    (rfid.uid.uidByte[i] < 0x10 ? "0" : "") +
+    String(rfid.uid.uidByte[i], HEX) +
+    (i!=3 ? ":" : "");
+  }
+  strID.toUpperCase();
+  /***FIM DO BLOCO DE CÓDIGO RESPONSÁVEL POR GERAR A TAG RFID LIDA***/
+
+  Serial.print("Identificador (UID) da tag: "); //IMPRIME O TEXTO NA SERIAL
+  Serial.println(strID); //IMPRIME NA SERIAL O UID DA TAG RFID
+  tone(buzzer,1500);
+  delay(500);
+  noTone(buzzer);
+  rfid.PICC_HaltA(); //PARADA DA LEITURA DO CARTÃO
+  rfid.PCD_StopCrypto1(); //PARADA DA CRIPTOGRAFIA NO PCD
+}
