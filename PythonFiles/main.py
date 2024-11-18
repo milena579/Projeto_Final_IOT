921a26528bc1ab8634662f58211b52e6c9ed1f39
juanoned <juandiego.jd42@gmail.com>
2024-11-18 17:11:32 -0300

    AA

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -19,7 +19,7 @@ def startSerial():
 def startModel():
     return YOLO("best.pt")
 
-def startDatabase():
+def startDatabase(tools):
     data = {"employers":
         [
             {
@@ -38,73 +38,98 @@ def startDatabase():
                 "RFID":"B3CA3310"
             },
 
-        ],"tools":
-        [
-            {
-                "name":"Hammer",
-                "qta":"1"
-            },
-            {
-                "name":"Pliers",
-                "qta":"1"
-            },
-            {
-                "name":"screwdriver",
-                "qta":"1"
-            },
-            {
-                "name":"wrench",
-                "qta":"2"
-            }
-        ]
+        ],"tools":tools,
+        "lend":-1
     }
     db.reference("/").set(data)
 
 def pushDatabase(data):
-    ref = db.reference("/lends").push(data)
+    db.reference("/tools").set(data)
 
 def captureImage():
     cap = cv2.VideoCapture(0)
-    status, photo = cap.read()
-    cap.release()
-    photo_resized = cv2.resize(photo, (640, 640))
-    return np.expand_dims(photo_resized, axis=0)
+    try:
+        status, photo = cap.read()
+        photo = cv2.resize(photo, (640, 640))
+        cv2.imwrite("image.png", photo)
+        return "image.png"
+    finally:
+        cap.release()
 
 def readSerial(ser):
     line = ser.readline()
     line = line.decode('utf-8')
+    return line
 
+def updateTools(tools):
+    result = model.predict(captureImage())    
+    for box in  result[0].boxes:
+        if(float(box.conf)>0.5):
+            tools[int(box.cls)]["qta"]+=1
+    pushDatabase(tools)
+    return tools
 
+def updateLending(data):
+    db.reference("/lend").push(data)
 
-
-
-
-
-
-
+def resetTools():
+    return [
+        {
+            "name":"Hammer",
+            "qta":0
+        },
+        {
+            "name":"Pliers",
+            "qta":0
+        },
+        {
+            "name":"screwdriver",
+            "qta":0
+        },
+        {
+            "name":"wrench",
+            "qta":0
+        }
+    ]
 
 connectFirebase()
 ser = startSerial()
 model = startModel()
-startDatabase()
 
+
+
+
+firstools = updateTools(resetTools())
+startDatabase(firstools)
 
 
 oldLine = ""
 
 while 1:
+    tools = resetTools()
+
     line = readSerial(ser)
+    print(line)
 
-    if(line!= "" or line!=oldLine):
+    if(line != "" or line!=oldLine):
         oldLine = line
-
-        result = model.predict(captureImage())
-        
-        print(result[0].names)
+        time.sleep(2)
+        result = model.predict(captureImage())    
         for box in  result[0].boxes:
-            print(box.cls,box.conf)
+            if(float(box.conf)>0.5):
+                tools[int(box.cls)]["qta"]+=1
+        pushDatabase(tools)
+    
+    toolLend=""
 
-        pushDatabase({"info":result[0].boxes})
+    for i in range(4):
+        num = firstools[i]["qta"] - tools[i]["qta"]
+        if(num>0):
+            toolLend += num+"x"+tools[i]["name"] if i==0 else ", "+num+"x"+tools[i]["name"]
+    if(toolLend==""):
+        continue
+    updateLending({"RFID":line,"loan":toolLend})
+
 
         
         
